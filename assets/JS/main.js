c585f2b6b07164696e01bb61321f656e6b9a852f
LoreFusillo <lorbeafus@gmail.com>
2025-12-09 12:17:22 -0300

    agregue los h1 q faltaban arme un archivo main.js y enlace ese archivo en index

---
+++ assets/JS/main.js
@@ -0,0 +1,7 @@
+import { initCartBadge } from './cart.js';
+import { initSearch } from './search.js';
+import { initFeaturedProducts } from './featuredProducts.js';
+
+initCartBadge();
+initSearch();
+initFeaturedProducts();
